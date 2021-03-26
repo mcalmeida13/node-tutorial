d36e61513148d52e5fa7142da295dbbeb77bc310
Manuela Carvalho <mcalmeida.13@gmail.com>
2021-03-26 14:30:30 -0300

    class 3

--- app.js
+++ app.js
@@ -1,5 +1,41 @@
 /*
 
+Class 3
+"""
+ReadLine Module
+"""
+*/
+const readline = require('readline');
+const rl = readline.createInterface({input: process.stdin,
+                          output: process.stdout});
+
+let num1 = Math.floor((Math.random()*10) + 1);
+let num2 = Math.floor((Math.random()*10) + 1);
+
+let answer = num1 + num2;
+
+rl.question(`What is ${ num1 } + ${ num2 }? \n`,
+(userInput) =>{
+    inputFunction(userInput,answer);
+});
+
+const inputFunction = function(userInput,answer) {
+    if(userInput.trim() == answer){
+        rl.close();
+    } else{
+        rl.setPrompt('Wrong answer! Try again \n')
+        rl.prompt();
+        rl.on('line',(c) => {
+            inputFunction(c,answer);
+        })
+    }
+}
+
+rl.on('close', () =>{
+    console.log("Correct!!");
+});
+/*
+
 Class 2
 """
 Event class
