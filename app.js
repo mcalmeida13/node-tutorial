1819d8f0e5772da9161fc96b10e1ee7f9e596867
Manuela Carvalho <mcalmeida.13@gmail.com>
2021-03-26 12:21:55 -0300

    add files

---
+++ app.js
@@ -0,0 +1,41 @@
+const EventEmitter = require('events');
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on('tutorial',(num1,num2) => {
+    console.log("tutorial event has occurred");
+    console.log(num1+num2);
+})
+
+eventEmitter.emit('tutorial',1,2);
+
+class Person extends EventEmitter{
+    constructor(name){
+        super();
+        this._name = name;
+    }
+
+    get name(){
+        return this._name;
+    }
+}
+
+console.log("tetse")
+let pedro = new Person('Pedro');
+pedro.on('name', ()=>{
+    console.log('my name is' + pedro.name);
+})
+/*
+"""
+Primeiro video
+"""
+const sum = require("./tutorial");
+
+// console.log("Node is working");
+const tutorial = require('./tutorial')
+// console.log(tutorial(1,1));
+
+console.log(tutorial);
+console.log(tutorial.sum(1,2));
+console.log(tutorial.PI);
+console.log(new tutorial.SomeMathObject());
+*/
---
+++ tutorial.js
@@ -0,0 +1,25 @@
+const sum = (num1,num2) => num1 + num2;
+
+const PI = 3.14;
+
+class SomeMathObject{
+    constructor(){
+        console.log("object created")
+    }
+}
+
+/*
+"""
+Aqui exporta um a um
+"""
+*/
+// module.exports.sum = sum;
+// module.exports.PI = PI;
+// module.exports.SomeMathObject=SomeMathObject;
+
+/*
+"""
+Aqui exporta todos de uma vez
+"""
+*/
+module.exports = {sum:sum, PI:PI, SomeMathObject:SomeMathObject}
